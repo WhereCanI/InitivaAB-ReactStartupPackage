b78e71fcbb3a7425f24c6b3b0530db5689973ad7
Michael Andersen <lefevreteam@gmail.com>
2020-12-22 10:16:58 +0100

    Adding support for JEST React Typescript component testing

---
+++ src/__tests__/WebAppContainer.test.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { render, fireEvent, waitForElement } from "@testing-library/react";
+import WebAppContainer from "../Components/WebAppContainer";
+
+describe("<WebAppContainer />", () => {
+    test("Render Web App Container, async () => {});
+});
